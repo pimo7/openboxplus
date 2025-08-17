70b312ebb5512f84de6fbb4c0a182fccb6cb56ea
pimo7 <pierre@chezromeo.com>
2025-08-17 02:23:09 -0400

    Add files via upload

---
+++ config/openbox/wallpaperchanger2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+while true
+do
+	feh --randomize   --bg-fill -d /home/$USER/wallpaper/* &
+    sleep 180
+done
+
+#MODES
+# --bg-fill 
+# --bg-center
+# --bg-scale
+# --bg-tile
+#  --auto-zoom
+#nano ./.fehbg
+
