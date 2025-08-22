c9b6a6acfef77935d6a1c3c79bf51feaaf447189
pimo7 <pierre@chezromeo.com>
2025-08-22 18:46:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install xfce4-appfinder terminator openbox jgmenu x-tile tint2 obconf xfce4-panel feh &
+sudo apt install xfce4-appfinder terminator openbox jgmenu x-tile tint2 obconf   feh &
 cp -r ./config/* /home/$USER/.config/ &
 mkdir /home/$user/wallpaper &
  
